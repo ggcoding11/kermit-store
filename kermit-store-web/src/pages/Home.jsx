06fa3feb056eadc68d9a81cbbfd79fdc0888196d
teste-bit-bucket-2-admin <gustavogomes1108@gmail.com>
2026-05-20 13:31:56 -0300

    #Feat: adição dos elementos de paginação

--- kermit-store-web/src/pages/Home.jsx
+++ kermit-store-web/src/pages/Home.jsx
@@ -19,6 +19,7 @@ import { formatCategory } from "../utils/FormatCategory";
 
 import Loading from "../components/Loading";
 import Header from "../components/Header";
+import Pagination from "react-js-pagination";
 
 import "../css/Home.css";
 import "react-responsive-modal/styles.css";
@@ -29,6 +30,7 @@ const Home = () => {
   const [products, setProducts] = useState(null);
 
   const [search, setSearch] = useState("");
+  const [page, setPage] = useState("");
   const [field, setField] = useState("");
   const [direction, setDirection] = useState("");
 
@@ -42,6 +44,7 @@ const Home = () => {
     getProducts({ search, field, direction })
       .then((products) => {
         setProducts(products.data);
+        setPage(products.data.pageable.pageNumber);
         setLoading(false);
       })
       .catch((error) => console.log(error));
@@ -117,143 +120,121 @@ const Home = () => {
             {loading ? (
               <Loading />
             ) : (
-              <div className="table-responsive">
-                <table className="table table-hover">
-                  <thead>
-                    <tr>
-                      <th scope="col">
-                        <div className="d-flex align-items-center gap-2">
-                          <span>#</span>
-                          <SortButton field="id" />
-                        </div>
-                      </th>
-                      <th scope="col">
-                        <div className="d-flex align-items-center gap-2">
-                          <span>Name</span>
-                          <SortButton field="name" />
-                        </div>
-                      </th>
-                      <th scope="col">
-                        <div className="d-flex align-items-center gap-2">
-                          <span>Brand</span>
-                          <SortButton field="brand" />
-                        </div>
-                      </th>
-                      <th scope="col">
-                        <div className="d-flex align-items-center gap-2">
-                          <span>Category</span>
-                          <SortButton field="category" />
-                        </div>
-                      </th>
-                      <th scope="col">
-                        <div className="d-flex align-items-center gap-2">
-                          <span>Created at</span>
-                          <SortButton field="creationDate" />
-                        </div>
-                      </th>
-                      <th scope="col">
-                        <div className="d-flex align-items-center gap-2">
-                          <span>Price</span>
-                          <SortButton field="price" />
-                        </div>
-                      </th>
-                      <th scope="col">
-                        <span>Actions</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {products &&
-                      products.map((product) => (
-                        <tr key={product.id}>
-                          <th scope="row">{product.id}</th>
-                          <td>{product.name}</td>
-                          <td>{product.brand}</td>
-                          <td>{formatCategory(product.category)}</td>
-                          <td>{product.creationDate}</td>
-                          <td>
-                            {new Intl.NumberFormat("pt-BR", {
-                              style: "currency",
-                              currency: "BRL",
-                            }).format(product.price)}
-                          </td>
-                          <td>
-                            <div className="d-flex gap-2">
-                              <motion.button
-                                whileHover={{ scale: 1.05 }}
-                                whileTap={{ scale: 0.95 }}
-                                className="btn btn-success"
-                                onClick={() => {
-                                  navigate(`/products/${product.id}`);
-                                }}
-                              >
-                                <BsEyeFill />
-                              </motion.button>
+              <div className="d-flex flex-column gap-2">
+                <div className="table-responsive">
+                  <table className="table table-hover">
+                    <thead>
+                      <tr>
+                        <th scope="col">
+                          <div className="d-flex align-items-center gap-2">
+                            <span>#</span>
+                            <SortButton field="id" />
+                          </div>
+                        </th>
+                        <th scope="col">
+                          <div className="d-flex align-items-center gap-2">
+                            <span>Name</span>
+                            <SortButton field="name" />
+                          </div>
+                        </th>
+                        <th scope="col">
+                          <div className="d-flex align-items-center gap-2">
+                            <span>Brand</span>
+                            <SortButton field="brand" />
+                          </div>
+                        </th>
+                        <th scope="col">
+                          <div className="d-flex align-items-center gap-2">
+                            <span>Category</span>
+                            <SortButton field="category" />
+                          </div>
+                        </th>
+                        <th scope="col">
+                          <div className="d-flex align-items-center gap-2">
+                            <span>Created at</span>
+                            <SortButton field="creationDate" />
+                          </div>
+                        </th>
+                        <th scope="col">
+                          <div className="d-flex align-items-center gap-2">
+                            <span>Price</span>
+                            <SortButton field="price" />
+                          </div>
+                        </th>
+                        <th scope="col">
+                          <span>Actions</span>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {products.content.length > 0 &&
+                        products.content.map((product) => (
+                          <tr key={product.id}>
+                            <th scope="row">{product.id}</th>
+                            <td>{product.name}</td>
+                            <td>{product.brand}</td>
+                            <td>{formatCategory(product.category)}</td>
+                            <td>{product.creationDate}</td>
+                            <td>
+                              {new Intl.NumberFormat("pt-BR", {
+                                style: "currency",
+                                currency: "BRL",
+                              }).format(product.price)}
+                            </td>
+                            <td>
+                              <div className="d-flex gap-2">
+                                <motion.button
+                                  whileHover={{ scale: 1.05 }}
+                                  whileTap={{ scale: 0.95 }}
+                                  className="btn btn-success"
+                                  onClick={() => {
+                                    navigate(`/products/${product.id}`);
+                                  }}
+                                >
+                                  <BsEyeFill />
+                                </motion.button>
 
-                              <motion.button
-                                whileHover={{ scale: 1.05 }}
-                                whileTap={{ scale: 0.95 }}
-                                className="btn btn-primary"
-                                onClick={() =>
-                                  navigate(`/update/${product.id}`)
-                                }
-                              >
-                                <BsPencilSquare />
-                              </motion.button>
+                                <motion.button
+                                  whileHover={{ scale: 1.05 }}
+                                  whileTap={{ scale: 0.95 }}
+                                  className="btn btn-primary"
+                                  onClick={() =>
+                                    navigate(`/update/${product.id}`)
+                                  }
+                                >
+                                  <BsPencilSquare />
+                                </motion.button>
 
-                              <motion.button
-                                whileHover={{ scale: 1.05 }}
-                                whileTap={{ scale: 0.95 }}
-                                className="btn btn-danger"
-                                onClick={() => {
-                                  onOpenModalDelete();
-                                  setIdToDelete(product.id);
-                                }}
-                              >
-                                <BsFillTrashFill />
-                              </motion.button>
-                            </div>
-                          </td>
-                        </tr>
-                      ))}
-                  </tbody>
-                </table>
+                                <motion.button
+                                  whileHover={{ scale: 1.05 }}
+                                  whileTap={{ scale: 0.95 }}
+                                  className="btn btn-danger"
+                                  onClick={() => {
+                                    onOpenModalDelete();
+                                    setIdToDelete(product.id);
+                                  }}
+                                >
+                                  <BsFillTrashFill />
+                                </motion.button>
+                              </div>
+                            </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                </div>
+
+                {products.totalPages > 0 && (
+                  <div className="d-flex justify-content-center">
+                    <button className="btn btn-secondary">&laquo;</button>
+                    <div className="p-2">
+                      <span>{page + 1}</span>
+                    </div>
+                    <button className="btn btn-secondary">&raquo;</button>
+                  </div>
+                )}
               </div>
             )}
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-12 d-flex justify-content-center">
-            <nav>
-              <ul className="pagination">
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    <span aria-hidden="true">&laquo;</span>
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    2
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    3
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    <span aria-hidden="true">&raquo;</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
           </div>
         </div>
       </section>
